330d5026da98e7d25190d3f5a35d4d9599fdfdef
Keanu Rouss <timajori1@gmail.com>
2025-07-09 01:37:18 -0400

    4th

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
-import React from "react";
-import SearchBar from "../SearchBar";
-
-function Header(){
-    
-}
+import style from "./Header.module.css"
+const Header = ({weather})=> {
+    return(
+        <div>
+        <h3>{weather.name}</h3>
+        <div className={style.temp}>{weather.main.temp}</div>
+        </div>
+    );
+};
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React ,{ useState } from "react";
 import SearchBar from "../components/SearchBar";
-
+import Header from "../components/Header";
 
 function Home() {
     const [weather, setweather] = useState(null)
@@ -8,7 +8,7 @@ function Home() {
          <div>
             <h1> Welcome Weather </h1>
             <SearchBar handleWeather={setweather} />
-            {weather ? <Header/>}
+            {weather ? <Header weather={weather}/> : 'No data Available'}
             </div>
     )
 }
